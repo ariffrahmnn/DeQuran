9544dd7b2c8a301e21f445ce7688628a9544d45d
ariffrahmnn <ariffrahmnn1@gmail.com>
2026-03-24 03:29:07 +0700

    Added fetching API using Axios

--- index.js
+++ index.js
@@ -3,11 +3,25 @@ import axios from 'axios';
 
 const app = express();
 const port = 3000;
+const API_URL = 'https://equran.id/api/v2/surat';
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-    res.render('index.ejs')
+app.get('/', async (req, res) => {
+    try {
+        const request = await axios.get(API_URL)
+        const surahList = request.data.data;
+    
+        const surahDatas = surahList.map(item => ({
+            title: item.namaLatin,
+            translate: item.arti
+        }))
+        res.render('index.ejs', {surahDatas: surahDatas})
+    } catch (error) {
+        console.error("Failed to make a request", error.message)
+        res.render ('index.ejs', {surahDatas: [],
+            error: error.message})
+    }
 })
 
 app.listen(port, () => {
